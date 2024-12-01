594724f80da9965f50f636967b774721cb4b1ef5
Satheesh-Baabu <satheeshbaabum@gmail.com>
2024-12-01 17:37:31 +0530

    change

--- src/Components/Developer.jsx
+++ src/Components/Developer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Developer() {
   var date =new Date;
   var year= date.getFullYear();
   return (
-    <div className='text-center text-white bg-gradient-to-r from-cyan-950 to-gray-800 p-3'>&copy; {year}&nbsp;Developed by <b className='text-cyan-400'><a href='/admin'>Satheesh Baabu</a></b>. All rights reserved. </div>
+    <div className='text-center text-white bg-gradient-to-r from-cyan-950 to-gray-800 p-3'>&copy; {year}&nbsp;Developed by <b className='text-cyan-400'><Link to='/admin'>Satheesh Baabu</Link></b>. All rights reserved. </div>
   )
 }
 
