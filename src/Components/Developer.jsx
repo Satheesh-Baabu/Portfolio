de80e27ed4f11fe942eca1b98371d11a3c3fb5af
Satheesh-Baabu <satheeshbaabum@gmail.com>
2024-12-01 17:33:45 +0530

    change

--- src/Components/Developer.jsx
+++ src/Components/Developer.jsx
@@ -4,7 +4,7 @@ function Developer() {
   var date =new Date;
   var year= date.getFullYear();
   return (
-    <div className='text-center text-white bg-gradient-to-r from-cyan-950 to-gray-800 p-3'>&copy; {year}&nbsp;Developed by <b className='text-cyan-400'><a href='/'>Satheesh Baabu</a></b>. All rights reserved. </div>
+    <div className='text-center text-white bg-gradient-to-r from-cyan-950 to-gray-800 p-3'>&copy; {year}&nbsp;Developed by <b className='text-cyan-400'><a href='/admin'>Satheesh Baabu</a></b>. All rights reserved. </div>
   )
 }
 
