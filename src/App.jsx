108ee1183c88456bf41056766fefe705227629a6
Satheesh-Baabu <satheeshbaabum@gmail.com>
2024-12-01 17:43:15 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route,Routes,BrowserRouter} from 'react-router-dom'
 import Admin from './pages/Admin'
 import AdminFeedback from './pages/AdminFeedback'
 import First from './pages/First'
+import Navbar from './Components/Navbar'
 
 function App() {
   
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      
+      <Navbar />
         <Routes>
           <Route path="/" element={<First/>}/>
           <Route path="/admin" element={<Admin/>}/>
