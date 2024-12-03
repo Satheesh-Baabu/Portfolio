526ac01000e499e6e111695e724bb7eeba15e26d
Satheesh-Baabu <satheeshbaabum@gmail.com>
2024-12-03 15:11:11 +0530

    resume changes

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Pdf from '/ResumeSatheesh.pdf'
 
 function Button(props) {
   return (
     <>
     {/* <button className='w-28 text-white bg-cyan-400 p-2 rounded-3xl border border-cyan-400 transition-all duration-300 ease-in-out hover:text-cyan-400 hover:bg-transparent'>{props.content}</button> */}
-    <a href=""><button className="relative px-6 py-3 font-semibold text-white bg-cyan-500 rounded-3xl overflow-hidden group hover:bg-transparent hover:scale-110 ease-in-out duration-700 group">
+    <a href={Pdf} target='_blank'>
+      <button className="relative px-6 py-3 font-semibold text-white bg-cyan-500 rounded-3xl overflow-hidden group hover:bg-transparent hover:scale-110 ease-in-out duration-700 group">
       <span className="relative z-10">{props.content}</span>
       <span className="absolute left-0 top-0 h-full w-full bg-cyan-400  transition-transform duration-700 ease-in-out transform -translate-x-full group-hover:translate-x-0"></span>
     </button></a>
